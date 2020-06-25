c6827a6960fe7767c7f941b9218b72470bcabf19
Юра Мельников <teejay3@MacBook.local>
2020-06-25 22:35:56 +0300

    cpp bubble sort

---
+++ sort/c++/bubble.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+void sort(std::vector<int>& arr)
+{
+    for(size_t i = 0; i < arr.size(); i++)
+        for(size_t j = 0; j < arr.size() - 1; j++)
+        {
+            if(arr[j] > arr[j + 1]) std::swap(arr[j], arr[j + 1]);
+        }
+}
